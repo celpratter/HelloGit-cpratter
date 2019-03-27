2ce37e1d5174f88a3e7438fec9ce83273b7c40c8
celpratter <celina.pratter@htl-salzburg.ac.at>
2019-03-27 14:59:19 +0100

    added canvas, text and tmp object; added script, first debug, change text from script

---
+++ Assets/MyGame/Scripts/FirstScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FirstScript : MonoBehaviour {
+
+    public Text myText;
+    public TextMeshProUGUI myTMPText;
+    
+	// Use this for initialization
+	void Start () {
+		myText.text = "cpratter";
+		myTMPText.text = "hello tmp text";
+		Debug.Log("Hello World");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
